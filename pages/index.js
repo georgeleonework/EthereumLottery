e822dc43d376899549f0a6c210e8865204ece6e3
georgeleonework <georgeleonework@gmail.com>
2022-08-25 12:48:46 -0400

    added content cards with bulma classes

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <nav className="navbar">
+        <nav className="navbar mt-4 mb-4">
           <div className="container">
             <div className="navbar-brand">
               <h1> Ether Lottery </h1>
@@ -26,10 +26,61 @@ export default function Home() {
           <section className="mt-5">
             <div className="columns">
               <div className="column is-two-thirds">
-                <p>Lottery Buttons</p>
+                <section className="mt-5">
+                  <p>Enter the Lottery by sending 0.01 Ether</p>
+                  <button className="button is-link is-large is-light mt-3">Play Now</button>
+                </section>
+                <section className="mt-6">
+                  <p><b>Admin only:</b> Pick winner</p>
+                  <button className="button is-primary is-large is-light mt-3">Pick Winner</button>
+                </section>
               </div>
               <div className="column is-one-third">
-                <p>Lottery Info</p>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Lottery History</h2>
+                        <div className="history-entry">
+                          <div>Lottery # 1 winner:</div>
+                          <div>
+                            <a href="https://etherscan.io/address/0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b" target="blank">0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Lottery History</h2>
+                        <div className="history-entry">
+                          <div>Lottery # 1 winner:</div>
+                          <div>
+                            <a href="https://etherscan.io/address/0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b" target="blank">0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Lottery History</h2>
+                        <div className="history-entry">
+                          <div>Lottery # 1 winner:</div>
+                          <div>
+                            <a href="https://etherscan.io/address/0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b" target="blank">0x0C777da66B91d2d9E43487D1Bb0C03Be67B83D9b</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
               </div>
             </div>
           </section>
